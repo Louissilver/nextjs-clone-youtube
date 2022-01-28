805246d400075c6ff899035d5745750721254450
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2022-01-28 19:03:11 -0300

    Material UI configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <div>
       Clone Youtube com Next JS e Material UI
+      <Button variant="outlined"> Olá Mundo</Button>
     </div>
   );
 }
